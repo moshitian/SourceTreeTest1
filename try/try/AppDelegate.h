71db984971983d91f7dc9cf4065d687cfa15c4c7
孙永鹏 <1219489753@qq.com>
2017-09-21 14:59:30 +0800

    都提交了

---
+++ try/try/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  try
+//
+//  Created by 李金蔚 on 17/9/21.
+//  Copyright © 2017年 SYP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ try/try/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  try
+//
+//  Created by 李金蔚 on 17/9/21.
+//  Copyright © 2017年 SYP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
